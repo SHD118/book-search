7c93fef0191e8fedccc60feb5cc04fdc7083c0e8
Hiro Protagonist <shdesai118@gmail.com>
2022-02-03 20:20:53 -0700

    all

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+	
+	// 	* `User` type:
+
+    //     * `_id`
+
+    //     * `username`
+
+    //     * `email`
+
+    //     * `bookCount`
+
+    //     * `savedBooks` (This will be an array of the `Book` type.)
+
+    // * `Book` type:
+
+    //     * `bookId` (Not the `_id`, but the book's `id` value returned from Google's Book API.)
+
+    //     * `authors` (An array of strings, as there may be more than one author.)
+
+    //     * `description`
+
+    //     * `title`
+
+    //     * `image`
+
+    //     * `link`
+
+    // * `Auth` type:
+
+    //     * `token`
+
+    //     * `user` (References the `User` type.)
